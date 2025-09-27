7d10d1c517c550bf92dac3bbe3ffaae6c9b6a5d8
leeseungryoun <fmjj008@gamil.com>
2025-09-27 16:18:10 +0900

    3일차-카트-스크롤바

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import Button from '@mui/material/Button';
-import { MdOutlineMenuOpen } from 'react-icons/md';
+import { MdOutlineMenuOpen, MdDarkMode } from 'react-icons/md';
 
 import SearchBox from '../SearchBox';
 import { MdOutlineLightMode } from 'react-icons/md';
-import { MdDarkMode } from 'react-icons/md';
 import { IoCartOutline } from 'react-icons/io5';
 import { MdOutlineMailOutline } from 'react-icons/md';
 import { FaRegBell } from 'react-icons/fa6';
@@ -16,15 +15,16 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
+import Box from '@mui/material/Box';
 
 import { IoShieldHalfSharp } from 'react-icons/io5';
 
 const Header = () => {
     const [anchorEl, setAnchorEl] = React.useState(null);
-
     const open = Boolean(anchorEl);
 
     const handleAccountClick = event => {
@@ -34,14 +34,40 @@ const Header = () => {
         setAnchorEl(null);
     };
 
-    const [regBellEl, setRegBellEl] = React.useState(null);
-    const openBell = Boolean(regBellEl);
+    const [bellEl, setBellEl] = React.useState(null);
+    const openBell = Boolean(bellEl);
 
-    const handleFaRegBellMenuClick = event => {
-        setAnchorEl(event.currentTarget);
+    const handleRegBellMenuClick = event => {
+        setBellEl(event.currentTarget);
     };
-    const handleFaRegBellMenuClose = () => {
-        setAnchorEl(null);
+    const handleRegBellMenuClose = () => {
+        setBellEl(null);
+    };
+
+    const [cartEl, setCartEl] = React.useState(null);
+    const openCart = Boolean(cartEl);
+
+    const handleCartMenuClick = event => {
+        setCartEl(event.currentTarget);
+    };
+    const handleCartMenuClose = () => {
+        setCartEl(null);
+    };
+
+    const [mailEl, setMailEl] = React.useState(null);
+    const openMail = Boolean(mailEl);
+
+    const handleMailMenuClick = event => {
+        setMailEl(event.currentTarget);
+    };
+    const handleMailMenuClose = () => {
+        setMailEl(null);
+    };
+
+    const [light, setLight] = React.useState(Boolean(false));
+
+    const handleLightMenuClick = event => {
+        setLight(light ? Boolean(false) : Boolean(true));
     };
 
     return (
@@ -65,78 +91,184 @@ const Header = () => {
                         </div>
 
                         <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
-                            <Button className="rounded-circle me-3">
-                                <MdOutlineLightMode />1
-                            </Button>
-                            <Button className="rounded-circle me-3">
-                                <MdDarkMode />2
-                            </Button>
-                            <Button className="rounded-circle me-3">
-                                <IoCartOutline />5
-                            </Button>
-                            <Button className="rounded-circle me-3">
-                                <MdOutlineMailOutline />3
-                            </Button>
+                            <div className=" d-flex align-items-center">
+                                <Button className="rounded-circle me-3" onClick={handleLightMenuClick}>
+                                    <MdOutlineLightMode />1
+                                </Button>
+                                <Button className="rounded-circle me-3">
+                                    <MdDarkMode />2
+                                </Button>
+                            </div>
 
-                            <Button className="rounded-circle me-3" onClick={handleFaRegBellMenuClick}>
-                                <FaRegBell />4
-                            </Button>
-
-                            <Menu
-                                anchorEl={anchorEl}
-                                id="FaRegBellMenu"
-                                open={openBell}
-                                onClose={handleFaRegBellMenuClose}
-                                onClick={handleFaRegBellMenuClose}
-                                slotProps={{
-                                    paper: {
-                                        elevation: 0,
-                                        sx: {
-                                            overflow: 'visible',
-                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                            mt: 1.5,
-                                            '& .MuiAvatar-root': {
-                                                width: 32,
-                                                height: 32,
-                                                ml: -0.5,
-                                                mr: 1,
-                                            },
-                                            '&::before': {
-                                                content: '""',
-                                                display: 'block',
-                                                position: 'absolute',
-                                                top: 0,
-                                                right: 14,
-                                                width: 10,
-                                                height: 10,
-                                                bgcolor: 'background.paper',
-                                                transform: 'translateY(-50%) rotate(45deg)',
-                                                zIndex: 0,
+                            <div className=" d-flex align-items-center">
+                                <Button className="rounded-circle me-3" onClick={handleCartMenuClick}>
+                                    <IoCartOutline />5
+                                </Button>
+                                <Menu
+                                    className="notifyItem"
+                                    anchorEl={cartEl}
+                                    id="cart-menu"
+                                    open={openCart}
+                                    onClose={handleCartMenuClose}
+                                    onClick={handleCartMenuClose}
+                                    slotProps={{
+                                        paper: {
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.5,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&::before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
                                             },
                                         },
-                                    },
-                                }}
-                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                            >
-                                <MenuItem onClick={handleFaRegBellMenuClose}>
-                                    <Avatar /> My account
-                                </MenuItem>
-                                <Divider />
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <div className="px-3 pb-0">
+                                        <h5>Orders(12)</h5>
+                                    </div>
+                                    <Divider className="mb-1" />
 
-                                <MenuItem onClick={handleFaRegBellMenuClose}>
-                                    <ListItemIcon>
-                                        <IoShieldHalfSharp />
-                                    </ListItemIcon>
-                                    Reset Password
-                                </MenuItem>
-                                <MenuItem onClick={handleFaRegBellMenuClose}>
-                                    <ListItemIcon>
-                                        <Logout />
-                                    </ListItemIcon>
-                                    Logout
-                                </MenuItem>
-                            </Menu>
+                                    <div className="itemList">
+                                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map(element => (
+                                            <MenuItem key={element} onClick={handleCartMenuClose}>
+                                                <div className="d-flex align-items-center">
+                                                    <div className="userImg">
+                                                        <span className="rounded-circle">
+                                                            <img src="https://mironcoder-hotash.netlify.app/images/avatar/01.webp" />
+                                                        </span>
+                                                    </div>
+                                                    <div className="dropInfo ms-2">
+                                                        Order #{element}
+                                                        <b>asdfasdf</b>
+                                                        asdfasdfasdf
+                                                        <b>dsfasdf</b>
+                                                        <p className="text-sky"> few secound ago</p>
+                                                    </div>
+                                                </div>
+                                            </MenuItem>
+                                        ))}
+                                    </div>
+                                </Menu>
+                            </div>
+
+                            <div className=" d-flex align-items-center">
+                                <Button className="rounded-circle me-3" onClick={handleMailMenuClick}>
+                                    <MdOutlineMailOutline />3
+                                </Button>
+                                <Menu
+                                    anchorEl={mailEl}
+                                    id="mail-menu"
+                                    open={openMail}
+                                    onClose={handleMailMenuClose}
+                                    onClick={handleMailMenuClose}
+                                    slotProps={{
+                                        paper: {
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.5,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&::before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem onClick={handleAccountClose}>
+                                        <ListItemIcon>
+                                            <IoShieldHalfSharp />
+                                        </ListItemIcon>
+                                        My Mail
+                                    </MenuItem>
+                                </Menu>
+                            </div>
+
+                            <div className=" d-flex align-items-center">
+                                <Button className="rounded-circle me-3" onClick={handleRegBellMenuClick}>
+                                    <FaRegBell />4
+                                </Button>
+                                <Menu
+                                    anchorEl={bellEl}
+                                    id="bell-menu"
+                                    open={openBell}
+                                    onClose={handleRegBellMenuClose}
+                                    onClick={handleRegBellMenuClose}
+                                    slotProps={{
+                                        paper: {
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.5,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&::before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem onClick={handleAccountClose}>
+                                        <ListItemIcon>
+                                            <IoShieldHalfSharp />
+                                        </ListItemIcon>
+                                        My Notifications
+                                    </MenuItem>
+                                </Menu>
+                            </div>
 
                             <div className="myAccWrapper d-flex align-items-center">
                                 <Button className="myAcc d-flex align-items-center" onClick={handleAccountClick}>
