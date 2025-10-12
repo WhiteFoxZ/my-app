21eaa983ebca074f5cb472c6108fbc1c7b72ba86
leeseungryoun <fmjj008@gamil.com>
2025-10-12 18:58:06 +0900

    6일차 왼쪽메뉴 완성2

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,143 +1,17 @@
-import React from 'react';
 import Button from '@mui/material/Button';
+import React from 'react';
 import { FaAngleRight } from 'react-icons/fa';
-import { MdDashboard } from 'react-icons/md';
-import { FaLock } from 'react-icons/fa';
-import { FaUserCircle } from 'react-icons/fa';
-import { AiFillProduct } from 'react-icons/ai';
 
-import { FaReceipt } from 'react-icons/fa6';
-
-import { FaShoppingCart } from 'react-icons/fa';
-import { MdEmail } from 'react-icons/md';
-import { FaBell } from 'react-icons/fa';
-import { FaGear } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
-import { BiFileBlank } from 'react-icons/bi';
-import { PiNoteBlankFill } from 'react-icons/pi';
 
 import { IoMdLogOut } from 'react-icons/io';
+
+import { SideBarMenuList } from '../SideBarMenuList/SideBarMenuList';
 
 const SideBar = () => {
     const [activeTap, setActiveTap] = React.useState(0);
 
-    const menuArray = [
-        {
-            id: 0,
-            name: 'Dashboard',
-            icon: <MdDashboard />,
-            submenu: [
-                { id: 1, name: 'Dashboard List', link: '/products' },
-                { id: 2, name: 'Dashboard View', link: '/add-product' },
-                { id: 3, name: 'Dashboard Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 1,
-            name: 'authentication',
-            icon: <FaLock />,
-            submenu: [
-                { id: 1, name: 'authentication List', link: '/products' },
-                { id: 2, name: 'authentication View', link: '/add-product' },
-                { id: 3, name: 'authentication Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 2,
-            name: 'users',
-            icon: <FaUserCircle />,
-            submenu: [
-                { id: 1, name: 'users List', link: '/products' },
-                { id: 2, name: 'users View', link: '/add-product' },
-                { id: 3, name: 'users Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 3,
-            name: 'Products',
-            icon: <AiFillProduct />,
-            submenu: [
-                { id: 1, name: 'users List', link: '/products' },
-                { id: 2, name: 'users View', link: '/add-product' },
-                { id: 3, name: 'users Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 4,
-            name: 'invoices',
-            icon: <FaReceipt />,
-            submenu: [
-                { id: 1, name: 'invoices List', link: '/products' },
-                { id: 2, name: 'invoices View', link: '/add-product' },
-                { id: 3, name: 'invoices Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 5,
-            name: 'Orders',
-            icon: <FaShoppingCart />,
-            submenu: [
-                { id: 1, name: 'Orders List', link: '/products' },
-                { id: 2, name: 'Orders View', link: '/add-product' },
-                { id: 3, name: 'Orders Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 6,
-            name: 'emails',
-            icon: <MdEmail />,
-            submenu: [
-                { id: 1, name: 'emails List', link: '/products' },
-                { id: 2, name: 'emails View', link: '/add-product' },
-                { id: 3, name: 'emails Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 7,
-            name: 'notifications',
-            icon: <FaBell />,
-            submenu: [
-                { id: 1, name: 'notifications List', link: '/products' },
-                { id: 2, name: 'notifications View', link: '/add-product' },
-                { id: 3, name: 'notifications Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 8,
-            name: 'settings',
-            icon: <FaGear />,
-            submenu: [
-                { id: 1, name: 'settings List', link: '/products' },
-                { id: 2, name: 'settings View', link: '/add-product' },
-                { id: 3, name: 'settings Upload', link: '/add-product' },
-            ],
-            isToggleSubmenu: false,
-        },
-
-        {
-            id: 9,
-            name: 'blank Page',
-            icon: <PiNoteBlankFill />,
-            submenu: [],
-            isToggleSubmenu: false,
-        },
-    ];
+    const menuArray = SideBarMenuList;
 
     const [menuList, setMenuList] = React.useState(menuArray);
 
---
+++ src/components/SideBarMenuList/SideBarMenuList.js
@@ -0,0 +1,130 @@
+import { AiFillProduct } from 'react-icons/ai';
+import { FaLock, FaUserCircle } from 'react-icons/fa';
+import { MdDashboard } from 'react-icons/md';
+
+import { FaReceipt } from 'react-icons/fa6';
+
+import { FaBell, FaShoppingCart } from 'react-icons/fa';
+import { FaGear } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
+import { PiNoteBlankFill } from 'react-icons/pi';
+
+export const SideBarMenuList = [
+    {
+        id: 0,
+        name: 'Dashboard',
+        icon: <MdDashboard />,
+        submenu: [
+            { id: 1, name: 'Dashboard List', link: '/products' },
+            { id: 2, name: 'Dashboard View', link: '/add-product' },
+            { id: 3, name: 'Dashboard Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 1,
+        name: 'authentication',
+        icon: <FaLock />,
+        submenu: [
+            { id: 1, name: 'authentication List', link: '/products' },
+            { id: 2, name: 'authentication View', link: '/add-product' },
+            { id: 3, name: 'authentication Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 2,
+        name: 'users',
+        icon: <FaUserCircle />,
+        submenu: [
+            { id: 1, name: 'users List', link: '/products' },
+            { id: 2, name: 'users View', link: '/add-product' },
+            { id: 3, name: 'users Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 3,
+        name: 'Products',
+        icon: <AiFillProduct />,
+        submenu: [
+            { id: 1, name: 'users List', link: '/products' },
+            { id: 2, name: 'users View', link: '/add-product' },
+            { id: 3, name: 'users Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 4,
+        name: 'invoices',
+        icon: <FaReceipt />,
+        submenu: [
+            { id: 1, name: 'invoices List', link: '/products' },
+            { id: 2, name: 'invoices View', link: '/add-product' },
+            { id: 3, name: 'invoices Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 5,
+        name: 'Orders',
+        icon: <FaShoppingCart />,
+        submenu: [
+            { id: 1, name: 'Orders List', link: '/products' },
+            { id: 2, name: 'Orders View', link: '/add-product' },
+            { id: 3, name: 'Orders Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 6,
+        name: 'emails',
+        icon: <MdEmail />,
+        submenu: [
+            { id: 1, name: 'emails List', link: '/products' },
+            { id: 2, name: 'emails View', link: '/add-product' },
+            { id: 3, name: 'emails Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 7,
+        name: 'notifications',
+        icon: <FaBell />,
+        submenu: [
+            { id: 1, name: 'notifications List', link: '/products' },
+            { id: 2, name: 'notifications View', link: '/add-product' },
+            { id: 3, name: 'notifications Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 8,
+        name: 'settings',
+        icon: <FaGear />,
+        submenu: [
+            { id: 1, name: 'settings List', link: '/products' },
+            { id: 2, name: 'settings View', link: '/add-product' },
+            { id: 3, name: 'settings Upload', link: '/add-product' },
+        ],
+        isToggleSubmenu: false,
+    },
+
+    {
+        id: 9,
+        name: 'blank Page',
+        icon: <PiNoteBlankFill />,
+        submenu: [],
+        isToggleSubmenu: false,
+    },
+];
+
+export default SideBarMenuList;
